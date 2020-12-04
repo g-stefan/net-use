22e8f6f17361c14ab7f30bf0801054695b4650c5
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:55:39 +0200

    update

--- port/build.msvc.clean-release.cmd
+++ port/build.msvc.clean-release.cmd
@@ -3,6 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> clean-local-release net-use
+echo -^> clean-release net-use
 
 if exist release\ rmdir /Q /S release
