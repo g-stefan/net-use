ef5595b7e474777cb5474528bd5689bf3c0c25a3
Grigore Stefan <g_stefan@yahoo.com>
2023-01-10 13:01:29 +0200

    Update

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Mount network drive
 
 ## License
 
-Copyright (c) 2022 Grigore Stefan
+Copyright (c) 2020-2023 Grigore Stefan
 Licensed under the [MIT](LICENSE) license.
--- source/XYO/NetUse/License.hpp
+++ source/XYO/NetUse/License.hpp
@@ -1,7 +1,7 @@
 // Net Use
-// Copyright (c) 2022 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
-// SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
 
 #ifndef XYO_NetUse_LICENSE_HPP
